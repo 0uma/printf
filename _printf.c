f1adb63032afddfb8c7a789f880651f4220c09f2
Oumaima <Cherifoumaima7119@gmail.com>
2023-10-15 15:21:16 -0700

    Updated printf

--- _printf.c
+++ _printf.c
@@ -42,9 +42,55 @@ int _printf(const char *format, ...)
 				_putchar(37);
 				count++;
 			}
+			else if (*format == 'd' || 'i')
+			{
+				int num = va_arg(args, int);
+				count += print_int(num);
+			}
 		}
 		format++;
 	}
 	va_end(args);
 	return (count);
+}
+int print_int(int num)
+{
+        int count = 0;
+
+        if (num < 0)
+        {
+                _putchar('-');
+                count++;
+                num = -num; 
+        }
+        if (num == 0)
+        {
+                _putchar('0');
+                count++;
+        }
+        else
+        {
+            int reverse_num = 0;
+            int num_digits = 0;
+            
+            int temp = num;
+            while (temp != 0)
+            {
+                num_digits++;
+                temp /= 10;
+            }
+            temp = num;
+            while (temp != 0)
+            {
+                reverse_num = reverse_num * 10 + temp % 10;
+                temp /= 10;
+            }
+            while (reverse_num != 0)
+            {
+                _putchar(reverse_num % 10 + '0');
+                count++;
+                reverse_num /= 10;
+            }
+        }
+        return count;
 }
