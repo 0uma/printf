7e9b3e897bc8be48c296a61a51213928e6e8f918
Oumaima <Cherifoumaima7119@gmail.com>
2023-10-16 16:07:31 -0700

    Updated printf function

--- format_type.c
+++ format_type.c
@@ -1,30 +1,37 @@
 #include "main.h"
 
+/**
+ * format_specifier - a function
+ * @specifier: a parameter
+ * @args: 2nd parameter
+ * Return: an int
+ */
+
 int format_specifier(char specifier, va_list args)
 {
-    int count;
+	int count;
 
-    count = 0;
-    if (specifier == 'c')
-        count += _putchar(va_arg(args, int));
-    else if (specifier == 's')
-    {
-        char *buffer = va_arg(args, char *);
-        int i = 0;
+	count = 0;
+	if (specifier == 'c')
+		count += _putchar(va_arg(args, int));
+	else if (specifier == 's')
+	{
+		char *buffer = va_arg(args, char *);
+		int i = 0;
 
-        for (; buffer[i] != '\0'; i++)
-            count += _putchar(buffer[i]);
-    }
-    else if (specifier == '%')
-    {
-        _putchar(37);
-        count++;
-    }
-    else if (specifier == 'd' || 'i')
-    {
-        int num = va_arg(args, int);
+		for (; buffer[i] != '\0'; i++)
+			count += _putchar(buffer[i]);
+	}
+	else if (specifier == '%')
+	{
+		_putchar(37);
+		count++;
+	}
+	else if (specifier == 'd' || 'i')
+	{
+		int num = va_arg(args, int);
 
-        count += print_int(num);
-    }
-    return (count);
+		count += print_int(num);
+	}
+	return (count);
 }
--- main.c
+++
@@ -1,1 +0,0 @@
-testDir/main.c
