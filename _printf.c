2ae6a4f70f31247d025dfb732140fa7078b85168
enDior Hillary <en.dior@outlook.com>
2023-10-16 01:01:58 +0100

    Refactored print_int function

--- _printf.c
+++ _printf.c
@@ -4,15 +4,12 @@
  * @format: a parameter
  * Return: returns an int
  */
-
 int _printf(const char *format, ...)
 {
-	int count;
+	int count = 0;
 	va_list args;
 
 	va_start(args, format);
-
-	count = 0;
 	while (*format != '\0')
 	{
 		if (*format != '%')
@@ -24,18 +21,14 @@ int _printf(const char *format, ...)
 		{
 			format++;
 			if (*format == 'c')
-			{
 				count += _putchar(va_arg(args, int));
-			}
 			else if (*format == 's')
 			{
 				char *buffer = va_arg(args, char *);
 				int i = 0;
 
 				for (; buffer[i] != '\0'; i++)
-				{
 					count += _putchar(buffer[i]);
-				}
 			}
 			else if (*format == '%')
 			{
@@ -45,6 +38,7 @@ int _printf(const char *format, ...)
 			else if (*format == 'd' || 'i')
 			{
 				int num = va_arg(args, int);
+
 				count += print_int(num);
 			}
 		}
@@ -52,45 +46,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (count);
-}
-int print_int(int num)
-{
-        int count = 0;
-
-        if (num < 0)
-        {
-                _putchar('-');
-                count++;
-                num = -num; 
-        }
-        if (num == 0)
-        {
-                _putchar('0');
-                count++;
-        }
-        else
-        {
-            int reverse_num = 0;
-            int num_digits = 0;
-            
-            int temp = num;
-            while (temp != 0)
-            {
-                num_digits++;
-                temp /= 10;
-            }
-            temp = num;
-            while (temp != 0)
-            {
-                reverse_num = reverse_num * 10 + temp % 10;
-                temp /= 10;
-            }
-            while (reverse_num != 0)
-            {
-                _putchar(reverse_num % 10 + '0');
-                count++;
-                reverse_num /= 10;
-            }
-        }
-        return count;
 }
--- main.h
+++ main.h
@@ -7,5 +7,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_int(int num);
 
 #endif
