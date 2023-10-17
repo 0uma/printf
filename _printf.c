faba3cd9854239bd14eae581280e1875c3b7ef22
enDior Hillary <en.dior@outlook.com>
2023-10-17 22:05:50 +0100

    Refactored: included exception in _printf function

--- _printf.c
+++ _printf.c
@@ -11,7 +11,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == NULL))
 		return (-1);
 
 	while (*format)
